6da8b8bed7f05c34b550ab6ad136cbc50de1c62f
Pergamene <smartz373@gmail.com>
2019-03-05 08:09:27 -0700

    created issue file

---
+++ src/markdown-to-partitions.js
@@ -0,0 +1,11 @@
+import { PARTITION_TYPES } from "./partition-types";
+
+let currentOutside = null;
+/**
+ * assemble all partition fragments
+ * 
+ * @param {string} markdownText see partitions-to-markdown 
+ */
+export function generatePartitions(markdownText) {
+  
+}
